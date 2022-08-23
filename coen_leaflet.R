83b0fbc5f94b765c996934d1cfe9c57d26c91811
RubenSchalk <rubenschalk1@gmail.com>
2022-08-23 14:56:38 +0200

    Add files via upload

--- coen_leaflet.R
+++ coen_leaflet.R
@@ -9,33 +9,49 @@ library(shiny)
 
 setwd("C:\\Users\\Schal107\\Documents\\UBU\\Coen_present")
 
-forms <- read.xlsx("coens_culinaire_reisgids_raw.xlsx", sheetIndex = 1)
+### These are all the steps, but you can start from the geocoded csv: ###
 
-google_key <- fread("C:\\Users\\Schal107\\Documents\\UBU\\Team DH\\Delpher\\google_key.txt")
-register_google(key = paste0(google_key$key))
+#forms <- read.xlsx("coens_culinaire_reisgids_raw.xlsx", sheetIndex = 1)
 
-coordinates <- geocode(forms$Horecagelegenheid..naam..adresgegevens.en.website....)
+#google_key <- fread("C:\\Users\\Schal107\\Documents\\UBU\\Team DH\\Delpher\\google_key.txt")
 
-forms <- cbind(forms, coordinates)
+#forms <- read.xlsx("coens_culinaire_reisgids_raw.xlsx", sheetIndex = 1)
+
+#coordinates <- geocode(forms$Horecagelegenheid..naam..adresgegevens.en.website....)
 
 # remove double entries
 
-forms <- forms[-12, ]
+#forms <- forms[-12, ]
 
-# add Germany for Sandra's entry
+# generate URL's that refer to the pages in the book
 
-setDT(forms)
-forms[is.na(lon), lon := 10,271483 ]
-forms[is.na(lat), lat := 51,095123 ]
+#forms$name_st <- gsub(" ", "-", tolower(forms$Name))
 
-# we can add historical world maps if we like, like this, and then add the url inside addTiles:
-# url <- ("https://maps.georeferencer.com/georeferences/5b0b6383-197e-5c57-a5a1-ec88d9b09154/2020-01-17T14:02:54.945310Z/map/{z}/{x}/{y}.png?key=ebGMmpORFAU1M65ypiIz")
+#forms$link <- paste0("https://kimbergmans.github.io/culinary-pilgrimage/", forms$name_st)
 
-leaflet(data = forms) %>% setView(lng = 10.95931966568949, lat = 50.1812298717116, zoom = 4) %>%
+## add Germany coordinates for Sandra's entry
+
+#setDT(forms)
+#forms[is.na(lon), lon := 10,271483 ]
+#forms[is.na(lat), lat := 51,095123 ]
+
+# save geocoded file with URLS:
+
+#fwrite(forms, "Coens_reisgids_full_geocoded.csv")
+
+### START HERE IF YOU ONLY WANT TO GENERATE THE MAP: ###
+
+forms <- fread("Coens_reisgids_full_geocoded.csv")
+
+### we can add historical world maps if we like, like this, and then add the url inside addTiles: ###
+
+#url <- ("https://maps.georeferencer.com/georeferences/c97256c1-26e3-5101-8282-384bff337213/2020-01-28T20:58:47.531031Z/map/{z}/{x}/{y}.png?key=ebGMmpORFAU1M65ypiIz")
+
+leaflet(data = forms) %>% setView(lng = 10.95931966568949, lat = 50.1812298717116, zoom = 2) %>%
   addTiles() %>%
   addCircleMarkers(lat = ~lat, lng = ~lon, popup = paste0("<a href='",forms$link,"'>",
-                                                          paste0(forms$Naam...,": ",forms$Dit.gerecht.zou.je.echt.eens.moeten.proberen..),"</a>"), 
+                                                          paste0(forms$Name,": ",forms$Dit.gerecht.zou.je.echt.eens.moeten.proberen..),"</a>"), 
                    color = "red", clusterOptions = markerClusterOptions())
 
 
-fwrite(forms, "Coens_reisgids_full_geocoded.csv")
+
